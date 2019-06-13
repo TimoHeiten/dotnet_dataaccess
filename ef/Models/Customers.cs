81237eea55c024fd94e58fb46f36cd4e25770b88
timo.heiten <heitech.heiten@gmail.com>
2019-06-13 23:18:47 +0200

    ef created

---
+++ ef/Models/Customers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace code.ef.Models
+{
+    public partial class Customers
+    {
+        public Customers()
+        {
+            Orders = new HashSet<Orders>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Lastname { get; set; }
+
+        public virtual ICollection<Orders> Orders { get; set; }
+    }
+}
